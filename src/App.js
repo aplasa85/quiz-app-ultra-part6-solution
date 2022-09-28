398e6baf6b624c1b8169c7bf6a75f45d6e111850
Axel Plasa <107868311+aplasa85@users.noreply.github.com>
2022-09-28 15:41:20 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/header/Header";
+import Navigation from "./components/navigation/Navigation";
+import Cards from "./pages/Cards";
+import Create from "./pages/Create";
+import Profile from "./pages/Profile";
+
+const initialCards = [
+  {
+    id: "b5db267b-3275-4a86-a9f4-e7f927d33ed0",
+    question: "Question 1",
+    answer: "Answer 1",
+    tags: ["Tag 1a", "Tag 1b", "Tag 1c"],
+    bookmarked: true,
+  },
+  {
+    id: "8f76114b-c30c-411d-ad38-77ce69079eef",
+    question: "Question 2",
+    answer: "Answer 2",
+    tags: ["Tag 2a", "Tag 2b", "Tag 2c"],
+    bookmarked: false,
+  },
+  {
+    id: "8c74c87b-2a68-4aa1-9733-4c6917dfdf88",
+    question: "Question 3",
+    answer: "Answer 3",
+    tags: ["Tag 3a", "Tag 3b", "Tag 3c"],
+    bookmarked: true,
+  },
+];
+
+function App() {
+  const [page, setPage] = useState("home");
+  const [cards, setCards] = useState(initialCards);
+
+  function appendCard(question, answer, tag) {
+    setCards((cards) => [
+      ...cards,
+      {
+        question,
+        answer,
+        tags: [tag],
+        id: Math.random().toString(36).substring(2),
+        bookmarked: false,
+      },
+    ]);
+
+    setPage("home");
+  }
+
+  function deleteCard(cardId) {
+    setCards((cards) => cards.filter(({ id }) => cardId !== id));
+  }
+
+  function toggleBookmark(cardId) {
+    setCards((cards) =>
+      cards.map((card) => ({
+        ...card,
+        bookmarked: cardId === card.id ? !card.bookmarked : card.bookmarked,
+      }))
+    );
+  }
+
+  return (
+    <div className="app">
+      <Header />
+      <main className="app__main">
+        {page === "home" && (
+          <Cards
+            cards={cards}
+            onDelete={deleteCard}
+            onBookmark={toggleBookmark}
+          />
+        )}
+        {page === "bookmark" && (
+          <Cards
+            cards={cards.filter((card) => card.bookmarked)}
+            onDelete={deleteCard}
+            onBookmark={toggleBookmark}
+          />
+        )}
+        {page === "create" && <Create onCreate={appendCard} />}
+        {page === "profile" && <Profile />}
+      </main>
+      <Navigation page={page} setPage={setPage} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Profile.js
@@ -0,0 +1,5 @@
+function Profile() {
+  return <p>Profile page is under construction</p>;
+}
+
+export default Profile;
